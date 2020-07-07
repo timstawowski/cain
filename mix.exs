807dbc7d948597fff42a5a2553b5abd9aaf9061f
Tim Stawowski <t.stawowski@wertgarantie.com>
2020-07-07 23:24:38 +0200

    Downgrade required elixir v

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cain.MixProject do
     [
       app: :cain,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
